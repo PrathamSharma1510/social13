b6c68829e4bb0e7fd8ac9213b9b310c333c17ef2
g-an24 <pratham.sharma6113@gmail.com>
2023-03-30 20:26:07 -0400

    class To function

--- src/components/homeVertical.js
+++ src/components/homeVertical.js
@@ -1,93 +1,81 @@
-import React from 'react'
-import styled from 'styled-components'
-
-import ListingResult from './listingResult'
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import ListingResult from './listingResult';
 
 const Title = styled.h1`
   text-align: center;
-`
+`;
 
 const Fieldset = styled.fieldset`
   text-align: center;
-`
+`;
 
 const Hint = styled.p`
   text-align: center;
   font-style: italic;
-`
+`;
 
-class HomeVertical extends React.Component {
-  state = {
-    searchField: "",
-    filterField: "tags",
-    listing: null
-  }
+function HomeVertical(props) {
+  const [searchField, setSearchField] = useState('');
+  const [filterField, setFilterField] = useState('tags');
+  const [listing, setListing] = useState(null);
 
-  changeHandler = event => {
-    const name = event.target.name
-    const value = event.target.value
-    this.setState({
-      [name]: value
-    })
-  }
+  const changeHandler = event => {
+    const { name, value } = event.target;
+    name === 'searchField' ? setSearchField(value) : setFilterField(value);
+  };
 
-  handleSubmit = event => {
-    event.preventDefault()
-    this.props.onSubmit && this.props.onSubmit(this.state)
-    
-    this.fetchAPI()
-    .then(res => {
-      this.setState({
-        listing: res
-      })
-    })
-    .catch( _ => console.log('no results'))
-  }
+  const handleSubmit = async event => {
+    event.preventDefault();
+    props.onSubmit && props.onSubmit({ searchField, filterField });
 
-  fetchAPI = async () => {
-    const res = await fetch('/api/listing/' + this.state.filterField + '/' + this.state.searchField)
-    const listing = await res.json()
-    
-    if(res.status !== 200) {
-      throw Error(listing.message)
+    try {
+      const res = await fetch(`/api/listing/${filterField}/${searchField}`);
+      const listingData = await res.json();
+
+      if (res.status !== 200) {
+        throw Error(listingData.message);
+      }
+
+      setListing(listingData);
+    } catch (err) {
+      console.log('no results');
     }
-    return listing
-  }
+  };
 
-  render() {
-    return (
-      <div>
-        <Title> Software Engineering SFSU Fall 2019 Section 1 Team 5</Title>
-        <form onSubmit={this.handleSubmit}>
-          <Fieldset>
-            <select value={this.state.filterField} name="filterField" onChange={this.changeHandler}>
-              <option value="tags">Tags</option>
-              <option value="zipcodes">Zipcode</option>
-            </select>
-            <label>
-              Search:
-              <input
-                type="text"
-                name="searchField"
-                placeholder="search"
-                value={this.state.searchField}
-                onChange={this.changeHandler}
-              />
-            </label>
-            <button type="submit">Search</button>
-          </Fieldset>
-        </form>
-        <Hint>
-          <b>valid test searches</b><br/>
-          <b>tags:</b> kitchen, pets, living room, haunted, parking<br/>
-          <b>zipcodes:</b> 94132, 94105<br/>
-        </Hint>
-        <ListingResult
-          value={this.state.listing}
-        />
-      </div>
-    )
-  }
+  return (
+    <div>
+      <Title>Software Engineering SFSU Fall 2019 Section 1 Team 5</Title>
+      <form onSubmit={handleSubmit}>
+        <Fieldset>
+          <select value={filterField} name="filterField" onChange={changeHandler}>
+            <option value="tags">Tags</option>
+            <option value="zipcodes">Zipcode</option>
+          </select>
+          <label>
+            Search:
+            <input
+              type="text"
+              name="searchField"
+              placeholder="search"
+              value={searchField}
+              onChange={changeHandler}
+            />
+          </label>
+          <button type="submit">Search</button>
+        </Fieldset>
+      </form>
+      <Hint>
+        <b>valid test searches</b>
+        <br />
+        <b>tags:</b> kitchen, pets, living room, haunted, parking
+        <br />
+        <b>zipcodes:</b> 94132, 94105
+        <br />
+      </Hint>
+      <ListingResult value={listing} />
+    </div>
+  );
 }
 
-export default HomeVertical
+export default HomeVertical;
